f94a07f20964a3b033055861df777b19515dc5b7
art <art@company.com>
2024-11-06 14:51:29 +0300

    viewinter

---
+++ src/gui/ViewInterface.h
@@ -0,0 +1,22 @@
+// ViewInterface.h
+#ifndef VIEWINTERFACE_H
+#define VIEWINTERFACE_H
+
+#include "../brick_game/defines.hpp"  // Структуры данных вашей игры
+
+class ViewInterface {
+public:
+    virtual ~ViewInterface() = default;
+
+    virtual void UpdateView(const GameInfo_t gameInfo) = 0;
+    virtual void DisplayPause() = 0;
+    virtual void ClearPause() = 0;
+    virtual void DisplayGameOver(const GameInfo_t &gameInfo) = 0;
+    virtual void DisplayGameWin() = 0;
+    virtual void DisplayOverlay() = 0;
+    virtual void DisplayNextTetramino(GameInfo_t gameInfo) = 0;
+
+    virtual void DisplayField() = 0;
+};
+
+#endif // VIEWINTERFACE_H
